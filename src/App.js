b15f401bc51776e407dd98e8ba2fcf6f526f9c9e
shafayat18 <shafayatplay@gmail.com>
2023-10-23 17:07:17 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/LoginForm/Login';
-
+import { users } from './components/FakeData/FakeData';
+import { Route, Routes } from 'react-router';
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/FakeData/FakeData.js
@@ -0,0 +1,89 @@
+
+export const users =[
+    {
+        id: 1,
+        name: 'John',
+        email: '5M6yj@example.com',
+        password: '12345'
+    },
+    {
+        id: 2,
+        name: 'Rhine',
+        email: 'rhine@com.com',
+        password: '12345'
+    },
+    {
+        id: 3,
+        name: 'Zakir',
+        email: 'zakir@kr.com',
+        password: '12345'
+    },
+    {
+        id: 4,
+        name: 'Rakib',
+        email: 'rakib@rk.com',
+        password: '12345'
+    },
+]
+
+export let parkingList = [
+    { 
+        id: 1,
+        carMake: "Toyota",
+        carModel: "Camry",
+        licensePlate: "ABC-123",
+        isOccupied: true,
+        entryTime: "2023-10-23 10:30:00",
+        exitTime: "2023-10-23 16:45:00",
+        entryUserId: 1
+    },
+    { 
+        id: 2,
+        carMake: "Honda",
+        carModel: "Civic",
+        licensePlate: "XYZ-789",
+        isOccupied: true,
+        entryTime: "2023-10-23 09:15:00",
+        exitTime: "2023-10-23 17:20:00",
+        entryUserId: 2
+    },
+    { 
+        id: 3,
+        carMake: "Ford",
+        carModel: "F-150",
+        licensePlate: "LMN-456",
+        isOccupied: false,
+        entryTime: null,
+        exitTime: null,
+        entryUserId: 1
+    },
+    
+    { 
+        id: 4,
+        carMake: "Honda",
+        carModel: "Civic",
+        licensePlate: "XYZ-789",
+        isOccupied: true,
+        entryTime: "2023-10-24 09:15:00",
+        exitTime: "2023-10-24 17:20:00",
+        entryUserId: 2
+    }, { 
+        id: 5,
+        carMake: "Honda",
+        carModel: "Civic",
+        licensePlate: "XYZ-789",
+        isOccupied: true,
+        entryTime: "2023-10-25 09:15:00",
+        exitTime: "2023-10-25 17:20:00",
+        entryUserId: 2
+    }, { 
+        id: 6,
+        carMake: "Honda",
+        carModel: "Civic",
+        licensePlate: "XYZ-789",
+        isOccupied: true,
+        entryTime: "2023-10-27 09:15:00",
+        exitTime: "2023-10-29 24:20:00",
+        entryUserId: 2
+    },
+];
