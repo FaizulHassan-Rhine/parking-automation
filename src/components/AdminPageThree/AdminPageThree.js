5623bfb2843b3c21e5c919db0d411f8786696970
shafayat18 <shafayatplay@gmail.com>
2023-10-27 15:42:57 +0600

    update

--- src/components/AdminPageThree/AdminPageThree.js
+++ src/components/AdminPageThree/AdminPageThree.js
@@ -130,7 +130,7 @@ const AdminPageThree = () => {
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                                 </svg>
                                             </div>
-
+                                            
                                             <input
                                                 onChange={searchFunc}
                                                 class=" h-full w-full outline-none text-sm text-gray-700 pr-2"
