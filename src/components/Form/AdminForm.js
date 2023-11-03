4a7e2d362072c618634a2d9ffe8be7dfbe170931
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-11-03 15:20:07 +0600

    add

--- src/components/Form/AdminForm.js
+++ src/components/Form/AdminForm.js
@@ -13,7 +13,7 @@ const AdminForm = () => {
     return (
         <>
 
-            <div className='bg-[#f5f5f5] pt-12 pb-12'>
+            <div className='bg-[#f5f5f5] pt-4 pb-6'>
                 <div className='container mx-auto'>
                     <div className='flex flex-col'>
                         <div className='flex justify-center pt-3'>
@@ -21,12 +21,12 @@ const AdminForm = () => {
                         </div>
                     </div>
                     <div className='flex flex-col items-center'>
-                        <div className='mt-11'>
+                        <div className='mt-4'>
                             <form onSubmit={singUpFunc} className='flex flex-col gap-10 p-10  w-[700px] bg-white rounded-lg shadow-sm2'>
                                 <div className='flex flex-col  gap-[20px]'>
 
                                     <div className='flex flex-col gap-[20px]'>
-                                        <h5 className='text-sm font-medium'>Packer
+                                        <h5 className='text-base font-medium'>Packer/Warehouse
                                         </h5>
 
                                         <div className={'relative w-full px-[14px] py-[10px] flex gap-2 border-[1px] border-solid rounded-lg border-[#98A2B3]'} >
@@ -35,33 +35,44 @@ const AdminForm = () => {
                                                 <option>Packer 1</option>
                                                 <option>Packer 2</option>
                                                 <option>Packer 3</option>
-                                                <option>Packer 4</option>
-                                                <option>Packer 5</option>
+                                                <option>Warehouse 1</option>
+                                                <option>Warehouse 2</option>
                                             </select>
                                         </div>
                                     </div>
 
                                     <div className='flex flex-col gap-[20px]'>
-                                        <h5 className='text-sm font-medium'>Product
+                                        <h5 className='text-base font-medium'>Product
                                         </h5>
 
                                         <div className={'relative w-full px-[14px] py-[10px] flex gap-2 border-[1px] border-solid rounded-lg border-[#98A2B3]'} >
                                             <select
                                                 className='w-full focus:outline-none bg-white'>
-                                                <option>Packer 1</option>
-                                                <option>Packer 2</option>
-                                                <option>Packer 3</option>
-                                                <option>Packer 4</option>
-                                                <option>Packer 5</option>
+                                                <option>Product 1</option>
+                                                <option>Product 2</option>
+                                                <option>Product 3</option>
+                                                <option>Product 4</option>
+                                               
                                             </select>
                                         </div>
                                     </div>
 
                                     <div className='flex flex-col gap-[20px]'>
-                                        <h5 className='text-sm font-medium'>Quantity
+                                        <h5 className='text-base font-medium'>Quantity
                                         </h5>
-                                        <div className={'relative px-[14px] py-[10px] flex gap-2 border-[1px] border-solid rounded-lg border-[#98A2B3] w-full'} >
-                                            <input className='w-full focus:outline-none' type="text" placeholder="Kg/ton" />
+                                        <div className="flex gap-5" >
+                                            <div className={'relative w-3/4 px-[14px] py-[10px] flex gap-2 border-[1px] border-solid rounded-lg border-[#98A2B3]'} >
+                                            <input className='w-full focus:outline-none' type="text" />
+                                            </div>
+                                            <div className={'relative w-1/4 px-[14px] py-[10px] flex gap-2 border-[1px] border-solid rounded-lg border-[#98A2B3]'} >
+                                            <select
+                                                className='w-full focus:outline-none bg-white'>
+                                                <option>KG</option>
+                                                <option>TON</option>
+                                             
+                                            </select>
+                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </div>
