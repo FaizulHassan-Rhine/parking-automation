d22666b6945c92f0d75081fffc7d8544c27a2436
shafayat18 <shafayatplay@gmail.com>
2023-11-07 16:54:23 +0600

    upddded

--- src/components/NavbarTest/NavbarTest.js
+++ src/components/NavbarTest/NavbarTest.js
@@ -124,9 +124,9 @@ const NavbarTest = () => {
                                     </div>
                                 </div>
                                 <div>
-
-                                    <div className={`dropdown dropdown-bottom navbar navbar-end 
-                                     ${getUserInfo.role == 'admin' ? "hidden lg:block" : ""}`}>
+                                    <div className={`dropdown dropdown-bottom navbar navbar-end flex gap-3 w-full
+                                     ${getUserInfo.role == 'admin' ? "hidden lg:flex" : ""}`}>
+                                        <div><p className='text-xs font-bold'>{getUserInfo.name} ({getUserInfo.displayRole})</p></div>
                                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                             <div className="w-10 rounded-full">
                                                 <img src={avatar} />
@@ -184,44 +184,44 @@ const NavbarTest = () => {
                                     )}
                                 </div>
 
-                                        {/* mobile menu triger button */}
-                                            
-                                        <div className={`${getUserInfo.role == 'admin' ? "lg:hidden" : "hidden"}`}>
-                                            <button
-                                                className="p-2 mt-[10px] text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                                                onClick={() => setNavbar(!navbar)}
+                                {/* mobile menu triger button */}
+
+                                <div className={`${getUserInfo.role == 'admin' ? "lg:hidden" : "hidden"}`}>
+                                    <button
+                                        className="p-2 mt-[10px] text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                        onClick={() => setNavbar(!navbar)}
+                                    >
+                                        {navbar ? (
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-6 h-6 text-black"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
                                             >
-                                                {navbar ? (
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="w-6 h-6 text-black"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                            clipRule="evenodd"
-                                                        />
-                                                    </svg>
-                                                ) : (
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="w-6 h-6 text-black"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                        strokeWidth={2}
-                                                    >
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            d="M4 6h16M4 12h16M4 18h16"
-                                                        />
-                                                    </svg>
-                                                )}
-                                            </button>
-                                        </div>
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        ) : (
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-6 h-6 text-black"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                strokeWidth={2}
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M4 6h16M4 12h16M4 18h16"
+                                                />
+                                            </svg>
+                                        )}
+                                    </button>
+                                </div>
 
                             </div>
                         </nav>
